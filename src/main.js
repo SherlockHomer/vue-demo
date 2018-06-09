7841b62d7fab4f5e0c10c681f8a1d207a7229d13
SherlockHomer <406435735@qq.com>
2018-06-09 16:17:22 +0800

    猜猜我是谁的代码

---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+
+var vmcheck = new Vue({
+    el:'#checkboxBox',
+    data:{
+        checked:[],
+        items:[
+        {name:'sher'},
+        {name:'jb杰城'},
+        {name:'homer'}
+        ]
+    }
+})
