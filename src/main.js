48e2be60cbf1e8bc41ee6de27cea0f4c645e630f
SherlockHomer <406435735@qq.com>
2018-06-09 21:59:10 +0800

    父子组件联系，子触发$emit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
-
-
-var vmcheck = new Vue({
-    el:'#checkboxBox',
-    data:{
-        checked:[],
-        items:[
-        {name:'sher'},
-        {name:'jb杰城'},
-        {name:'homer'}
-        ]
+Vue.component('counter',{
+    data:function(){
+        return {
+            count:0
+        }
+    },
+    template:'<button @click="countMe" :value="count">you click me to {{count}} times</button>',
+    methods:{
+        countMe:function(e){
+            this.count++;
+            this.$emit('changefont',this.count);
+        }
     }
+});
+
+new Vue({
+    el:'#counters-demo',
+    data:{
+        clickTimes:1
+    }
+    // methods:{
+    //     changefont:function(times){
+    //         this.clickTimes = times;
+    //     }
+    // }
 })
