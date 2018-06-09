9675b4037b011deb2b24c9401e9be03bc93a30d4
SherlockHomer <406435735@qq.com>
2018-06-09 16:56:45 +0800

    ugly todo list

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
+Vue.component('todo-item',{
+    template:'<li class="form-group" :key="todo.id">{{todo.id}}.{{todo.text}}<button class="btn-danger" @click="$emit(\'remove\')">-</button></li>',
+    props:['todo']
+});
 
-
-var vmcheck = new Vue({
-    el:'#checkboxBox',
+var vm = new Vue({
+    el:'#todo-list',
     data:{
-        checked:[],
-        items:[
-        {name:'sher'},
-        {name:'jb杰城'},
-        {name:'homer'}
+        newTodoText:'',
+        newTodoId:4,
+        todos:[
+        {id:1,text:'get up early'},
+        {id:2,text:'clean teeth'},
+        {id:3,text:'eat breakfast'}
         ]
+    },
+    methods:{
+        addNewOne:function(){
+            let newTodo = this.todos[this.todos.length-1] ;
+            newTodoId = newTodo ? newTodo.id+1 : 1;
+            this.todos.push({id:newTodoId,text:this.newTodoText})
+        }
     }
 })
