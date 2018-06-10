44c0e6e4e239803d52a343879ca5a254d48a9b03
SherlockHomer <406435735@qq.com>
2018-06-10 21:30:54 +0800

    传递到组件，v-model的vm理解

--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
-
+Vue.component('base-checkbox',{
+    props:['check','index'],
+    template:`
+        <div>
+            <input type="checkbox" :id="index" :value="value" v-model="checked"/>
+            <label :for="index">{{checkedValue}}</label>
+        </div>
+    `,
+    computed:{
+        checkedValue:function(){
+            // 这里改变prop 的值
+            return this.checked ? this.value : '猜猜我是谁';
+        }
+    },
+    data:function(){
+        return {
+            checked:this.check.checked,
+            value:this.check.value
+        }
+    }
+})
 
 var vmcheck = new Vue({
     el:'#checkboxBox',
     data:{
-        checked:[],
-        items:[
-        {name:'sher'},
-        {name:'jb杰城'},
-        {name:'homer'}
+        checkboxs:[
+        {value:'sher',checked:true},
+        {value:'lock',checked:false},
+        {value:'homer',checked:true}
         ]
     }
 })
